e3100d2e86518ee75657736fb5f17a0647f88643
yukuri611 <yukiiw611@gmail.com>
2025-05-26 20:00:03 +0900

    LSTMを実装

--- common/util.py
+++ common/util.py
@@ -58,19 +58,6 @@ def ppmi(C, verbose, eps = 1e-8):
     
     return M
             
-    
-
-
-def clip_grads(grads, max_norm):
-    total_norm = 0
-    for grad in grads:
-        total_norm += np.sum(grad ** 2)
-    total_norm = np.sqrt(total_norm)
-
-    rate = max_norm / (total_norm + 1e-6)
-    if rate < 1:
-        for grad in grads:
-            grad *= rate
 
 
 def create_contexts_target(corpus, window_size=1):
@@ -110,3 +97,60 @@ def convert_one_hot(corpus, vocab_size):
                 one_hot[idx_0, idx_1, word_id] = 1
 
     return one_hot
+
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
+
+
+def eval_perplexity(model, corpus, batch_size=10, time_size=35):
+    print('evaluating perplexity ...')
+    corpus_size = len(corpus)
+    total_loss = 0
+    max_iters = (corpus_size - 1) // (batch_size * time_size)
+    jump = (corpus_size - 1) // batch_size
+
+    for iters in range(max_iters):
+        xs = np.zeros((batch_size, time_size), dtype=np.int32)
+        ts = np.zeros((batch_size, time_size), dtype=np.int32)
+        time_offset = iters * time_size
+        offsets = [time_offset + (i * jump) for i in range(batch_size)]
+        for t in range(time_size):
+            for i, offset in enumerate(offsets):
+                xs[i, t] = corpus[(offset + t) % corpus_size]
+                ts[i, t] = corpus[(offset + t + 1) % corpus_size]
+
+        try:
+            loss = model.forward(xs, ts, train_flg=False)
+        except TypeError:
+            loss = model.forward(xs, ts)
+        total_loss += loss
+
+        sys.stdout.write('\r%d / %d' % (iters, max_iters))
+        sys.stdout.flush()
+
+    print('')
+    ppl = np.exp(total_loss / max_iters)
+    return ppl
+
+def to_cpu(x):
+    import numpy
+    if type(x) == numpy.ndarray:
+        return x
+    return np.asnumpy(x)
+
+
+def to_gpu(x):
+    import cupy
+    if type(x) == cupy.ndarray:
+        return x
+    return cupy.asarray(x)
+
